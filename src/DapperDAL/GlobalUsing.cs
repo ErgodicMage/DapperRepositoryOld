e8c79166318266797ccc98cbb8b69425dca2d963
Ergodic Mage <ErgodicMage@gmail.com>
2022-08-23 23:48:39 -0500

    Removerd Implicit Using (stupid fing option) from project file. Updated GlobalUsing appropriately.

--- src/DapperDAL/GlobalUsing.cs
+++ src/DapperDAL/GlobalUsing.cs
@@ -1,7 +1,9 @@
 ﻿global using System;
 global using System.Collections.Generic;
 global using System.Data;
+global using System.Linq;
 global using System.Reflection;
 global using System.Text;
+global using System.Threading.Tasks;
 
 global using Dapper;
