ff0642e49a9d6889ee5045ebac2eb9ff96664c0b
Ergodic Mage <ErgodicMage@gmail.com>
2022-08-30 12:05:00 -0500

    Fixed CategoryRepository for UpdateImage functions.

--- Examples/Northwind/NorthwindRepository/CategoryRepository.cs
+++ Examples/Northwind/NorthwindRepository/CategoryRepository.cs
@@ -52,41 +52,41 @@ public class CategoryRepository : GenericRepository<Category, int>, ICategoriesR
 
     public int UpdateImage(int id, byte[] image, int? commandTimeout = null)
     {
-        return Update(new { CategoryID = id }, new { Picture = image }, commandTimeout);
+        return Update(id, new { Picture = image }, commandTimeout);
     }
 
     public int UpdateImage(IDbConnection connection, int id, byte[] image, IDbTransaction? transaction = null, int? commandTimeout = null)
     {
-        return Update(connection, new { CategoryID = id }, new { Picture = image }, transaction, commandTimeout);
+        return Update(connection, id, new { Picture = image }, transaction, commandTimeout);
     }
 
     public Task<int> UpdateImageAync(int id, byte[] image, int? commandTimeout = null)
     {
-        return UpdateAsync(new { CategoryID = id }, new { Picture = image }, commandTimeout);
+        return UpdateAsync(id, new { Picture = image }, commandTimeout);
     }
 
     public Task<int> UpdateImageAync(IDbConnection connection, int id, byte[] image, IDbTransaction? transaction = null, int? commandTimeout = null)
     {
-        return UpdateAsync(connection, new { CategoryID = id }, new { Picture = image }, transaction, commandTimeout);
+        return UpdateAsync(connection, id, new { Picture = image }, transaction, commandTimeout);
     }
 
     public int UpdateDescription(int id, string description, int? commandTimeout = null)
     {
-        return Update(new { CategoryID = id }, new { Description = description }, commandTimeout);
+        return Update(id, new { Description = description }, commandTimeout);
     }
 
     public int UpdateDescription(IDbConnection connection, int id, string description, IDbTransaction? transaction = null, int? commandTimeout = null)
     {
-        return Update(connection, new { CategoryID = id }, new { Description = description }, transaction, commandTimeout);
+        return Update(connection, id, new { Description = description }, transaction, commandTimeout);
     }
 
     public Task<int> UpdateDescriptionAync(int id, string description, int? commandTimeout = null)
     {
-        return UpdateAsync(new { CategoryID = id }, new { Description = description }, commandTimeout);
+        return UpdateAsync(id, new { Description = description }, commandTimeout);
     }
 
     public Task<int> UpdateDescriptionAync(IDbConnection connection, int id, string description, IDbTransaction? transaction = null, int? commandTimeout = null)
     {
-        return UpdateAsync(connection, new { CategoryID = id }, new { Description = description }, transaction, commandTimeout);
+        return UpdateAsync(connection, id, new { Description = description }, transaction, commandTimeout);
     }
 }
